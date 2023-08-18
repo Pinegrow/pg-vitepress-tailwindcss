84971308f308e532251eecceb04aacfd36f6e8f4
Akayy <techakayy@gmail.com>
2023-08-18 15:01:24 +1000

    Minor updates

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -8,13 +8,21 @@
 //   window.devtools = devtools
 // }
 
-// https://vitepress.dev/guide/custom-theme
+// For customizing theme, make sure you install vue as a dependency
+// Refer to: https://vitepress.dev/guide/custom-theme
+// Refer to: https://vitepress.dev/guide/extending-default-theme#layout-slots
+
+// You can directly import Vue files in the theme entry
+// VitePress is pre-configured with @vitejs/plugin-vue.
+// import Layout from './layouts/default.vue'
+
+import 'uno.css'
+
 // import { h } from 'vue'
 // import Theme from 'vitepress/theme'
 import Layout from '@/layouts/default.vue'
 // import '@/assets/css/main.css'
 import '@/assets/css/tailwind.css'
-import 'uno.css'
 
 export default {
   // extends: Theme,
--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -12,15 +12,32 @@ import presetIcons from '@unocss/preset-icons'
 export default defineConfig({
   plugins: [
     liveDesigner({
-      devtoolsKey: 'devtools',
+      iconPreferredCase: 'unocss', // default value (can be removed), unocss by default uses the unocss format for icon names
+      devtoolsKey: 'devtools', // see app.ts
       tailwindcss: {
+        /* Please ensure that you update the filenames and paths to accurately match those used in your project. */
         configPath: '../tailwind.config.ts',
         cssPath: '@/tailwind.css',
       },
+      // plugins: [
+      //   {
+      //     name: 'My Awesome Lib 3.0',
+      //     key: 'my-awesome-lib',
+      //     pluginPath: fileURLToPath(
+      //       new URL('./my-awesome-lib/web-types.json', import.meta.url),
+      //     ),
+      //   },
+      // ],
     }),
     // ...
     // For details, refer to https://github.com/antfu/unplugin-auto-import#configuration
     AutoImportAPIs({
+      include: [
+        /.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /.vue$/,
+        /.vue?vue/, // .vue
+        /.md$/, // .md
+      ],
       imports: [
         'vue',
         'vue-router',
@@ -31,6 +48,7 @@ export default defineConfig({
         // 'pinia',
       ],
       dirs: [
+        /* Please ensure that you update the filenames and paths to accurately match those used in your project. */
         // 'src/composables',
         // 'src/stores',
       ],
@@ -38,8 +56,18 @@ export default defineConfig({
       dts: 'auto-imports.d.ts',
     }),
     // ...
+    // For details, refer to https://github.com/antfu/unplugin-vue-components#configuration
     AutoImportComponents({
-      dirs: ['.vitepress/theme/components'],
+      /* Take care to amend any paths to reflect the actual paths used in your project. */
+
+      dirs: ['./.vitepress/theme/components'],
+
+      // allow auto load markdown components under ./src/components/
+      extensions: ['vue', 'md'],
+
+      // allow auto import and register components used in markdown
+      include: [/.vue$/, /.vue?vue/, /.md$/],
+
       // resolvers: [], // Auto-import using resolvers
       dts: 'components.d.ts',
     }),
@@ -57,6 +85,8 @@ export default defineConfig({
     alias: {
       /* Must be either an object, or an array of { find, replacement, customResolver } pairs. */
       /* Refer to: https://vitejs.dev/config/shared-options.html#resolve-alias */
+      /* Please ensure that you update the filenames and paths to accurately match those used in your project. */
+
       '@': fileURLToPath(new URL('./.vitepress/theme', import.meta.url)),
       '~': fileURLToPath(new URL('./.vitepress/theme', import.meta.url)),
       '~~': fileURLToPath(new URL('./.vitepress', import.meta.url)),
