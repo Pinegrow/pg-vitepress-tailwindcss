c5a709156d5611cc342a51e70e08c3ef2e3e5fda
Akayy <techakayy@gmail.com>
2024-06-06 19:05:25 +1000

    Initial commit

---
+++ pinegrow.config.cjs
@@ -0,0 +1,9 @@
+//pinegrow.config.cjs
+
+module.exports = {
+  liveDesigner: {
+    /* Please ensure that you update the filenames and paths to accurately match those used in your project. */
+    configPath: './docs/.vitepress/config.mts',
+    //... existing config
+  },
+}
